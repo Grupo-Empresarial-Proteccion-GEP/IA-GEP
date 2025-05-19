fae00d68c2045196e06bf1faffe0e6936f63d6b8
diego.caceres <diego.caceres123>
2025-05-19 16:59:59 -0500

    Agregar archivos de proyecto.

---
+++ QrBlazorServices/Controller/QrCode.cs
@@ -0,0 +1,11 @@
+﻿namespace QrBlazorServices.Controller
+{
+    public class QrCode
+    {
+        string Nombre = "";
+        string Cargo = "";
+        string Telefono = "";
+        string Empresa = "";
+        string QRBase64;
+    }
+}
---
+++ QrBlazorServices/Controller/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace QrBlazorServices.Controller
+{
+    public class UsuarioModel
+    {
+        public string Nombre { get; set; }
+        public string Documento { get; set; }
+        public string Cargo { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
