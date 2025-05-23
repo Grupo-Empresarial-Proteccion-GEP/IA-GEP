bb850832df09c5d1bd795df32a3814c681824895
diego.caceres <diego.caceres123>
2025-05-23 13:50:21 -0500

    MAS

--- QrBlazorServices/wwwroot/js/Carnet.js
+++ QrBlazorServices/wwwroot/js/Carnet.js
@@ -21,6 +21,31 @@
     });
 };
 
+window.CarnetOPT = (elementId, fileName) => {
+    const element = document.getElementById(elementId);
+    if (!element) {
+        alert("No se encontró el carnet para descargar.");
+        return;
+    }
+
+    html2canvas(element, {
+        backgroundColor: "#000000",
+        scale: 1,
+        width: 326,
+        height: 205
+    }).then(canvas => {
+        const link = document.createElement("a");
+        link.download = fileName + ".png";
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+    }).catch(error => {
+        alert("Error al generar imagen: " + error);
+        console.error(error);
+    });
+};
+
+
+
 window.descargarCarnetComoPDF = async function () {
     const element = document.getElementById("carnetContainer");
     if (!element) return;
@@ -38,3 +63,4 @@ window.descargarCarnetComoPDF = async function () {
     pdf.addImage(imgData, "PNG", 0, 0, 86, 54);
     pdf.save("Carnet.pdf");
 };
+
---
+++ QrBlazorServices/wwwroot/js/imageHelper.js
@@ -0,0 +1,44 @@
+﻿// wwwroot/js/imageHelper.js
+
+export async function corregirOrientacionYConvertir(base64String) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = function () {
+            EXIF.getData(img, function () {
+                const orientation = EXIF.getTag(this, "Orientation") || 1;
+
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                let width = img.width;
+                let height = img.height;
+
+                if ([5, 6, 7, 8].includes(orientation)) {
+                    canvas.width = height;
+                    canvas.height = width;
+                } else {
+                    canvas.width = width;
+                    canvas.height = height;
+                }
+
+                switch (orientation) {
+                    case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+                    case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+                    case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+                    case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+                    case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+                    case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+                    case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+                    default: break;
+                }
+
+                ctx.drawImage(img, 0, 0);
+
+                const base64Corregido = canvas.toDataURL('image/jpeg');
+                resolve(base64Corregido);
+            });
+        };
+        img.onerror = reject;
+        img.src = base64String;
+    });
+}
