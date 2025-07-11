4f14e071a05a010b927bd3f22dbd256c0cc32715
DiegoCaceresDs2 <Desarrollador2@gep.com.co>
2025-07-11 12:48:14 -0500

    Creacion Modulo registro Categoria/Subcategoria

---
+++ QrBlazorServices/Clases/SubCategoria.cs
@@ -0,0 +1,9 @@
+﻿namespace QrBlazorServices.Clases
+{
+    public class SubCategoria
+    {
+        public int Id { get; set; }
+        public int CategoriaId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ QrBlazorServices/Clases/categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace QrBlazorServices.Clases
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
