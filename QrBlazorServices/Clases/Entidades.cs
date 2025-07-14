bb13feafd5f915c49cfe4ce176b8254a65b01a1a
DiegoCaceresDs2 <Desarrollador2@gep.com.co>
2025-07-14 10:23:35 -0500

    Correcion de entidades y modulo Plantillas

--- QrBlazorServices/Clases/Entidades.cs
+++ QrBlazorServices/Clases/Entidades.cs
@@ -3,16 +3,18 @@
     public class Entidad
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Tipo { get; set; }
-        public string TipoDocumento { get; set; }
-        public string NumeroDocumento { get; set; }
-        public string Departamento { get; set; }
-        public string Ciudad { get; set; }
-        public string Direccion { get; set; }
-        public string Telefono { get; set; }
-        public string Correo { get; set; }
+        public string? Nombre { get; set; }
+        public int TipoEntidadId { get; set; }
+        public string? Tipo { get; set; } // ← para el nombre del tipo
+        public string? TipoDocumento { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public string? Departamento { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
     }
+
 
     public class Subdivision
     {
--- QrBlazorServices/Clases/TipoEntidadModel.cs
+++ QrBlazorServices/Clases/TipoEntidadModel.cs
@@ -3,7 +3,8 @@
     public class TipoEntidadModel
     {
         public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
+        public string Nombre { get; set; } = "";
     }
+
 
 }
