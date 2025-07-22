2ea0cb8eee804f3e40f923230e57d62c0fadab46
DiegoCaceresDs2 <Desarrollador2@gep.com.co>
2025-07-22 16:58:04 -0500

    Nuevo Registro

--- QrBlazorServices/Clases/NodoCategoria.cs
+++ QrBlazorServices/Clases/NodoCategoria.cs
@@ -6,12 +6,11 @@
         public string Nombre { get; set; } = "";
         public int? PadreId { get; set; }
         public string? NombreIndentado { get; set; } // ✅ Usado solo para mostrar en DropDown
+        public bool EsCategoria { get; set; }
+        public bool Expanded { get; set; } = false;
+        public List<NodoCategoria> Hijos { get; set; } = new List<NodoCategoria>();
+        public bool EsNodoBuscado { get; set; } = false; // 🌟 Para resaltar
+        public string NombreOriginal { get; set; }
 
-
-     
-
-            public bool EsCategoria { get; set; }
-            public List<NodoCategoria> Hijos { get; set; } = new();
-        
     }
 }
