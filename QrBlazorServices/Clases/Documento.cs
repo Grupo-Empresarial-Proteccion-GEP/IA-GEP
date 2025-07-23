d12f93f6bc6ad59b06ad2c651be988c886c9c9f6
DiegoCaceresDs2 <Desarrollador2@gep.com.co>
2025-07-23 16:11:38 -0500

    registro nEW

--- QrBlazorServices/Clases/Documento.cs
+++ QrBlazorServices/Clases/Documento.cs
@@ -5,5 +5,7 @@
         public string? Nombre { get; set; }
         public string? Referencia { get; set; }
         public int? PlantillaId { get; set; }
+        public int? NodoCategoriaId { get; set; }
+
     }
 }
