ec2570eca0d81cbb86658f2854f466c5b51d75b9
DiegoCaceresDs2 <Desarrollador2@gep.com.co>
2025-07-28 12:28:23 -0500

    registro documento funcional completo

--- QrBlazorServices/Clases/Documento.cs
+++ QrBlazorServices/Clases/Documento.cs
@@ -6,6 +6,8 @@
         public string? Referencia { get; set; }
         public int? PlantillaId { get; set; }
         public int? NodoCategoriaId { get; set; }
+        public string Version { get; set; }
+        public string Estado { get; set; }
 
     }
 }
